24953222d389abbb1f629c1ac390a937ff8261ff
TakanashiHitomi <sf470506412@gmail.com>
2018-04-14 01:26:51 +0800

    Feat:Show snack-bar when click;
    Signed-off-by: TakanashiHitomi <sf470506412@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'toast_kit.dart';
 
 void main() => runApp(new MyApp());
 
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Vultr Panel',
       theme: new ThemeData(
         // This is the theme of your application.
         //
@@ -17,14 +18,14 @@ class MyApp extends StatelessWidget {
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
         // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Vultr Panel Title'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends Scaffold {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -42,7 +43,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends ScaffoldState {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -68,37 +69,52 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text('widget.title'),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+
+      /// When the [Scaffold] is actually created in the same `build` function, the
+      /// `context` argument to the `build` function can't be used to find the
+      /// [Scaffold] (since it's "above" the widget being returned). In such cases,
+      /// the following technique with a [Builder] can be used to provide a new
+      /// scope with a [BuildContext] that is "under" the [Scaffold]:
+      body: new Builder(builder: (BuildContext builderContext) {
+        return new Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: new Column(
+            // Column is also layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug paint" (press "p" in the console where you ran
+            // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+            // window in IntelliJ) to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                'You have pushed the button this many times:',
+              ),
+              new Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.display1,
+              ),
+              new MaterialButton(
+                child: Text('snack bar'),
+                onPressed: () => Scaffold.of(builderContext).showSnackBar(
+                    new SnackBar(
+                        content:
+                            new Text('Button is pressed $_counter times'))),
+              ),
+            ],
+          ),
+        );
+      }),
       floatingActionButton: new FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
